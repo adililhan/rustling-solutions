3835143e18461aac568b96157e0f426390a1323d
adil ilhan <info@adililhan.com>
2022-09-18 13:58:54 +0000

    initial commit

---
+++ exercises/clippy/target/debug/deps/clippy3-b0ec142db6d0c1a4.d
@@ -0,0 +1,7 @@
+/root/rustlings/exercises/clippy/target/debug/deps/clippy3-b0ec142db6d0c1a4.rmeta: clippy3.rs
+
+/root/rustlings/exercises/clippy/target/debug/deps/clippy3-b0ec142db6d0c1a4.d: clippy3.rs
+
+clippy3.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
---
+++ exercises/error_handling/errors4.rs
@@ -0,0 +1,42 @@
+// errors4.rs
+// Execute `rustlings hint errors4` or use the `hint` watch subcommand for a hint.
+
+
+#[derive(PartialEq, Debug)]
+struct PositiveNonzeroInteger(u64);
+
+#[derive(PartialEq, Debug)]
+enum CreationError {
+    Negative,
+    Zero,
+}
+
+impl PositiveNonzeroInteger {
+    fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
+        match value {
+            x if x == 0 => Err(CreationError::Zero),
+            x if x < 0 => Err(CreationError::Negative),
+            x => Ok(PositiveNonzeroInteger(x as u64))
+        }
+        /*
+        if value == 0 {
+            Err(CreationError::Zero)
+        } else if value < 0 {
+            Err(CreationError::Negative)
+        } else {
+            // Hmm...? Why is this only returning an Ok value?
+            Ok(PositiveNonzeroInteger(value as u64))
+        }
+        */
+    }
+}
+
+#[test]
+fn test_creation() {
+    assert!(PositiveNonzeroInteger::new(10).is_ok());
+    assert_eq!(
+        Err(CreationError::Negative),
+        PositiveNonzeroInteger::new(-10)
+    );
+    assert_eq!(Err(CreationError::Zero), PositiveNonzeroInteger::new(0));
+}
